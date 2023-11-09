b8108c604a724e8743fd94ca4dbc2695ccc31ad7
ZaharyN <124094259+ZaharyN@users.noreply.github.com>
2023-11-09 19:56:33 +0200

    Update Configuration.cs

--- C# Database Course/Entity Framework Core/Advanced Querying Exercise/BookShop.Data/Configuration.cs
+++ C# Database Course/Entity Framework Core/Advanced Querying Exercise/BookShop.Data/Configuration.cs
@@ -3,6 +3,6 @@
     internal class Configuration
     {
         internal static string ConnectionString
-            => "Server=ZAHARY;Database=BookShop;Integrated Security=True;";
+            => "Server=******;Database=BookShop;Integrated Security=True;";
     }
 }
