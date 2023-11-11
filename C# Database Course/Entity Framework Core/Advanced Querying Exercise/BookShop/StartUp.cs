b92f4eb42d6037a9d4df22ed8ec8cd99cdf6b5b7
ZaharyN <124094259+ZaharyN@users.noreply.github.com>
2023-11-11 13:53:19 +0200

    Update StartUp.cs

--- C# Database Course/Entity Framework Core/Advanced Querying Exercise/BookShop/StartUp.cs
+++ C# Database Course/Entity Framework Core/Advanced Querying Exercise/BookShop/StartUp.cs
@@ -11,9 +11,10 @@
             using var db = new BookShopContext();
            // DbInitializer.ResetDatabase(db);
 
-            Console.WriteLine(GetBooksByAgeRestriction(db,"Sk"));
+            Console.WriteLine(GetBooksNotReleasedIn(db, 2000));
         }
 
+        //02.
         public static string GetBooksByAgeRestriction(BookShopContext context, string command)
         {
             if (!Enum.TryParse<AgeRestriction>(command, true, out var ageRestriction))
@@ -31,6 +32,58 @@
                 .ToList();
 
             return string.Join(Environment.NewLine, books.Select(b => b.Title));
+        }
+
+        //03.
+        public static string GetGoldenBooks(BookShopContext context)
+        {
+            var goldenBooks = context.Books
+                .Where(b => b.EditionType == EditionType.Gold
+                    && b.Copies < 5000)
+                .Select(b => new
+                {
+                    b.Title,
+                    b.BookId
+                })
+                .ToList();
+        
+            return string.Join(Environment.NewLine, goldenBooks
+                .OrderBy(b => b.BookId)
+                .Select(b => b.Title));
+        }
+
+        //04.
+        public static string GetBooksByPrice(BookShopContext context)
+        {
+            var books = context.Books
+                .Where(b => b.Price >= 40.00m)
+                .Select(b => new 
+                { 
+                    b.Title,
+                    b.Price
+                });
+        
+            return string.Join(Environment.NewLine, books
+                .OrderByDescending(b => b.Price)
+                .Select(b => $"{b.Title} - ${b.Price:f2}"));
+        }
+
+        //05.
+        public static string GetBooksNotReleasedIn(BookShopContext context, int year)
+        {
+            var nonReleasedBooks = context.Books
+                .Where(b => b.ReleaseDate.HasValue 
+                    && b.ReleaseDate.Value.Year != year)
+                .Select(b => new
+                {
+                    b.Title,
+                    b.BookId
+                })
+                .ToList();
+        
+            return string.Join(Environment.NewLine, nonReleasedBooks
+                .OrderBy(b => b.BookId)
+                .Select(b => b.Title));
         }
     }
 }
