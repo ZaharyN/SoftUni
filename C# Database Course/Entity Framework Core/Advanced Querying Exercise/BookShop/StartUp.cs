a3a3901ca90a5756bd6317d92232f33a3698679c
ZaharyN <124094259+ZaharyN@users.noreply.github.com>
2023-11-12 22:12:34 +0200

    Update StartUp.cs

--- C# Database Course/Entity Framework Core/Advanced Querying Exercise/BookShop/StartUp.cs
+++ C# Database Course/Entity Framework Core/Advanced Querying Exercise/BookShop/StartUp.cs
@@ -3,23 +3,25 @@
     using BookShop.Models.Enums;
     using Data;
     using Initializer;
+    using System.Globalization;
+    using System.Net.Mime;
 
     public class StartUp
     {
         public static void Main()
         {
             using var db = new BookShopContext();
-           // DbInitializer.ResetDatabase(db);
+            //DbInitializer.ResetDatabase(db);
 
-            Console.WriteLine(GetBooksNotReleasedIn(db, 2000));
+            Console.WriteLine(GetBooksByCategory(db, "horror mystery drama"));
         }
 
         //02.
         public static string GetBooksByAgeRestriction(BookShopContext context, string command)
         {
-            if (!Enum.TryParse<AgeRestriction>(command, true, out var ageRestriction))
+            if (!Enum.TryParse<AgeRestriction>(command, true, out AgeRestriction ageRestriction))
             {
-                return $"{command} is not a valid type";
+                return $"{command} is not a valid type!";
             }
 
             var books = context.Books
@@ -28,10 +30,12 @@
                 {
                     b.Title
                 })
-                .OrderBy(b => b.Title)
                 .ToList();
 
-            return string.Join(Environment.NewLine, books.Select(b => b.Title));
+
+            return string.Join(Environment.NewLine, books
+                    .OrderBy(b => b.Title)
+                    .Select(b => b.Title));
         }
 
         //03.
@@ -46,7 +50,7 @@
                     b.BookId
                 })
                 .ToList();
-        
+
             return string.Join(Environment.NewLine, goldenBooks
                 .OrderBy(b => b.BookId)
                 .Select(b => b.Title));
@@ -57,12 +61,12 @@
         {
             var books = context.Books
                 .Where(b => b.Price >= 40.00m)
-                .Select(b => new 
-                { 
+                .Select(b => new
+                {
                     b.Title,
                     b.Price
                 });
-        
+
             return string.Join(Environment.NewLine, books
                 .OrderByDescending(b => b.Price)
                 .Select(b => $"{b.Title} - ${b.Price:f2}"));
@@ -72,7 +76,7 @@
         public static string GetBooksNotReleasedIn(BookShopContext context, int year)
         {
             var nonReleasedBooks = context.Books
-                .Where(b => b.ReleaseDate.HasValue 
+                .Where(b => b.ReleaseDate.HasValue
                     && b.ReleaseDate.Value.Year != year)
                 .Select(b => new
                 {
@@ -80,10 +84,42 @@
                     b.BookId
                 })
                 .ToList();
-        
+
             return string.Join(Environment.NewLine, nonReleasedBooks
                 .OrderBy(b => b.BookId)
                 .Select(b => b.Title));
+        }
+
+        //06.
+        public static string GetBooksByCategory(BookShopContext context, string input)
+        {
+            string[] categories = input
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            var booksByCategories = context.Books
+                .Where(b => b.BookCategories.Any(bc => categories.Contains(bc.Category.Name)))
+                .Select(b => new
+                {
+                    b.Title
+                })
+                .ToList();
+
+            return string.Join(Environment.NewLine, booksByCategories
+                    .OrderBy(b => b.Title)
+                    .Select(b => b.Title));
+        }
+
+        //07.
+        public static string GetBooksReleasedBefore(BookShopContext context, string date)
+        {
+            DateTime parsedDate = 
+
+            //var booksBeforeGivenYear = context.Books
+            //    .Where(b => b.ReleaseDate.HasValue
+            //        && b.ReleaseDate.Value.Year < date.)
+
+            return string.Empty;
         }
     }
 }
