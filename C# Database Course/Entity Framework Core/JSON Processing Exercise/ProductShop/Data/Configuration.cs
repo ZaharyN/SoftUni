9119e3a65501e7f1c96032cf3b433c6db96218c9
ZaharyN <124094259+ZaharyN@users.noreply.github.com>
2023-11-16 21:29:42 +0200

    Update Configuration.cs

--- C# Database Course/Entity Framework Core/JSON Processing Exercise/ProductShop/Data/Configuration.cs
+++ C# Database Course/Entity Framework Core/JSON Processing Exercise/ProductShop/Data/Configuration.cs
@@ -3,6 +3,6 @@
     public static class Configuration
     {
         public const string ConnectionString =
-            @"Server=DESKTOP-GMV9351\SQLEXPRESS;Database=ProductShop;Integrated Security=True";
+            @"Server=******;Database=ProductShop;Integrated Security=True";
     }
 }
