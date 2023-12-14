90db58461b63aabe7e6c744fc913bfa10e54d1e6
ZaharyN <124094259+ZaharyN@users.noreply.github.com>
2023-12-14 19:06:09 +0200

    Functional Programming lab

---
+++ C# Advanced Course/C# Advanced/Functional Programming/01/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Functional_Programming_Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, int> parser = s => int.Parse(s);
+            Func<int, bool> filter = n => n % 2 == 0;
+            Func<int, int> selector = n => n;
+
+            int[] input = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(parser)
+                .OrderBy(selector)
+                .ToArray();
+
+            int[] sorted = input.Where(filter).ToArray();
+
+            Console.WriteLine(string.Join(", ", sorted));
+        }
+    }
+}
---
+++ C# Advanced Course/C# Advanced/Functional Programming/02/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace SumNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Func<int[], int> sum = x => x.Sum();
+            Func<int[], int> length = Length;
+
+
+            Console.WriteLine(length(input));
+            Console.WriteLine(sum(input));
+            //Console.WriteLine(input.Length);
+            //Console.WriteLine(input.Sum());
+        }
+
+        //static int Sum(int[] input)
+        //{
+        //    int sum = 0;
+        //    foreach (int element in input)
+        //    {
+        //        sum += element;
+        //    }
+
+        //    return sum;
+        //}
+
+        static int Length(int[] input)
+        {
+            int length = 0;
+
+            foreach (var element in input)
+            {
+                length++;
+            }
+            return length;
+        }
+    }
+}
