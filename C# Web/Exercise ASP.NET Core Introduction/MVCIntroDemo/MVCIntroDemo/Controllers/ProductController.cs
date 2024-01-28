1382d81188b384597ea4dd2f235dc4e70a58f8e5
ZaharyN <124094259+ZaharyN@users.noreply.github.com>
2024-01-29 00:51:07 +0200

    Update

---
+++ C# Web/Exercise ASP.NET Core Introduction/MVCIntroDemo/MVCIntroDemo/Controllers/ProductController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCIntroDemo.Models.Product;
+
+namespace MVCIntroDemo.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IEnumerable<ProductViewModel> products = new List<ProductViewModel>()
+        {
+            new ProductViewModel()
+            {
+                Id = 1,
+                Name = "Cheese",
+                Price = 7.00
+            },
+            new ProductViewModel()
+            {
+                Id = 2,
+                Name = "Hame",
+                Price = 5.50
+            },
+            new ProductViewModel()
+            {
+                Id = 3,
+                Name = "Bread",
+                Price = 1.50
+            }
+        };
+        
+        public IActionResult All()
+        {
+            return View(products);
+        }
+    }
+}
