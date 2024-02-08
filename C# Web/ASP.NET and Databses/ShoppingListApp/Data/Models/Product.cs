1cebeb5ae01dd93ca9aa2b8e5c009227466dadf7
ZaharyN <124094259+ZaharyN@users.noreply.github.com>
2024-02-09 00:10:56 +0200

    Added exercise ShoppingListDbContext

---
+++ C# Web/ASP.NET and Databses/ShoppingListApp/Data/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace ShoppingListApp.Data.Models
+{
+    [Comment("Shopping List Product")]
+	public class Product
+	{
+        [Comment("Product Id")]
+        [Key]
+        public int Id { get; set; }
+
+        [Comment("Name of product")]
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; } = string.Empty;
+
+        public List<ProductNote> ProductNotes { get; set; } = new List<ProductNote>();
+    }
+}
---
+++ C# Web/ASP.NET and Databses/ShoppingListApp/Data/Models/ProductNote.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ShoppingListApp.Data.Models
+{
+    [Comment("Product Note")]
+	public class ProductNote
+	{
+        [Key]
+        [Comment("Note Identifier")]
+        public int Id { get; set; }
+
+        [Comment("Note Content")]
+        [Required]
+        [StringLength(150)]
+        public string Coantent { get; set; } = string.Empty;
+
+        [Comment("Product Identifier")]
+        [Required]
+        public int ProductId { get; set; }
+
+        [ForeignKey(nameof(ProductId))]
+        public Product Product { get; set; } = null!;
+    }
+}
