92075cff2acda82367bfccf0186a03af64d2068b
ZaharyN <zlnyagolov@abv.bg>
2024-02-09 17:09:06 +0200

    Added WorkShopApp

---
+++ C# Web/Workshop - Forum App/ForumApp/Data/Configuration/PostConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using ForumApp.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ForumApp.Data.Configuration
+{
+    public class PostConfiguration : IEntityTypeConfiguration<Post>
+    {
+        public void Configure(EntityTypeBuilder<Post> builder)
+        {
+            //builder.HasData()
+        }
+    }
+}
---
+++ C# Web/Workshop - Forum App/ForumApp/Data/ForumAppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using ForumApp.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ForumApp.Data
+{
+    public class ForumAppDbContext : DbContext
+    {
+        public ForumAppDbContext(DbContextOptions<ForumAppDbContext> options)
+            : base(options)
+        {
+            
+        }
+
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
