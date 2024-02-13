44e92cde837c358e894d8856ce520a2554ca41d2
ZaharyN <124094259+ZaharyN@users.noreply.github.com>
2024-02-13 19:50:04 +0200

    Commit Workshop - ForumApp

---
+++ C# Web/Workshop - ForumApp/Data/ForumAppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using ForumApp.Data.Configuration;
+using ForumApp.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ForumApp.Data
+{
+    public class ForumAppDbContext : DbContext
+    {
+        public ForumAppDbContext(DbContextOptions<ForumAppDbContext> options)
+            : base(options)
+        {
+            
+        }
+
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration<Post>(new PostConfiguration());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
