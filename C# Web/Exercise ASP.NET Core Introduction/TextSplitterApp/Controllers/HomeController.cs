020655fa041fef68e5462655a15f5a6fa90bcaa9
ZaharyN <124094259+ZaharyN@users.noreply.github.com>
2024-02-15 22:32:40 +0200

    Add Workshop TaskboardApp

---
+++ C# Web/Exercise ASP.NET Core Introduction/TextSplitterApp/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TextSplitterApp.Models;
+
+namespace TextSplitterApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(TextViewModel model)
+        {
+            return View(model);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Split(TextViewModel model)
+        {
+            var splitText = model
+                .Text
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            model.SplitText = string.Join(Environment.NewLine, splitText);
+
+            return RedirectToAction("Index", model);
+        }
+    }
+}
