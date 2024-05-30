4ccb09d434e448bc0ccda8ded9ffc803c795d53e
ZaharyN <124094259+ZaharyN@users.noreply.github.com>
2024-05-31 00:25:10 +0300

    Added two more tasks

---
+++ C# Algorithms/01. Recursion and backtracking/Paths in Labyrinth/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Paths_in_Labyrinth
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			List<string> path = new List<string>();
+			int rows = int.Parse(Console.ReadLine());
+			int cols = int.Parse(Console.ReadLine());
+
+			char[,] lab = new char[rows, cols];
+
+			for (int i = 0; i < lab.GetLength(0); i++)
+			{
+				string column = Console.ReadLine();
+
+				for (int j = 0; j < lab.GetLength(1); j++)
+				{
+					lab[i, j] = column[j];
+				}
+			}
+
+			FindPath(lab, 0, 0, path, string.Empty);
+		}
+
+		public static void FindPath(char[,] lab, int row, int col, List<string> path, string direction)
+		{
+			if (row < 0 || row >= lab.GetLength(0) || col < 0 || col >= lab.GetLength(1))
+			{
+				return;
+			}
+
+			if (lab[row, col] == '*')
+			{
+				return;
+			}
+
+			if (lab[row, col] == 'e')
+			{
+				path.Add(direction);
+				Console.WriteLine(string.Join("", path));
+				path.RemoveAt(path.Count - 1);
+				return;
+			}
+
+			if (lab[row, col] == '-' && lab[row, col] != 'M')
+			{
+				path.Add(direction);
+				lab[row, col] = 'M';
+
+				FindPath(lab, row - 1, col, path, "U");
+				FindPath(lab, row, col + 1, path, "R");
+				FindPath(lab, row, col - 1, path, "L");
+				FindPath(lab, row + 1, col, path, "D");
+
+				path.RemoveAt(path.Count - 1);
+				lab[row, col] = '-';
+			}
+		}
+	}
+}
---
+++ C# Algorithms/01. Recursion and backtracking/Recursive Factorial/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Recursive_Factorial
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int n = int.Parse(Console.ReadLine());
+
+			Console.WriteLine(CalculateFactorial(n));
+		}
+
+		public static int CalculateFactorial(int n)
+		{
+			if(n == 0) return 1;
+
+			return n * CalculateFactorial(n - 1);
+		}
+	}
+}
