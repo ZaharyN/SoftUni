c3452ba72d8c96ad93662997a03f985690b96280
ZaharyN <124094259+ZaharyN@users.noreply.github.com>
2024-06-09 00:28:33 +0300

    Added tasks for Combinatorial problems

---
+++ C# Algorithms/02. Combinatorial Problems/Permutations with  Repetition Reduced Complexity/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Permutations_with__Repetition_Reduced_Complexity
+{
+	internal class Program
+	{
+		public static char[] elements;
+		static void Main(string[] args)
+		{
+			elements = Console.ReadLine()
+				.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+				.Select(char.Parse)
+				.ToArray();
+
+			GeneratePermutations(0);
+		}
+
+		private static void GeneratePermutations(int index)
+		{
+			if (index >= elements.Length)
+			{
+				Console.WriteLine(string.Join(" ", elements));
+				return;
+			}
+
+			GeneratePermutations(index + 1);
+
+			for (int i = index + 1; i < elements.Length; i++)
+			{
+				SwapPlaces(index, i);
+				GeneratePermutations(index + 1);
+				SwapPlaces(index, i);
+			}
+		}
+
+		private static void SwapPlaces(int currentNumberIndex, int nextNumber)
+		{
+			char temp = elements[currentNumberIndex];
+
+			elements[currentNumberIndex] = elements[nextNumber];
+			elements[nextNumber] = temp;
+		}
+	}
+}
---
+++ C# Algorithms/02. Combinatorial Problems/Permutations with Repetition/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Permutations_with_Repetition
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, World!");
+ 		}
+	}
+}
