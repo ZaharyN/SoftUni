96718e73d8d07c520e7475b14502c0af1ebea529
ZaharyN <124094259+ZaharyN@users.noreply.github.com>
2024-06-08 18:04:33 +0300

    Added Recursive Fibonacci task with recursion

---
+++ C# Algorithms/01. Recursion and backtracking/Recursive Fibonacci/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Recursive_Fibonacci
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int index = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(CalculateFibonacci(index));
+
+		}
+
+		private static long CalculateFibonacci(int index)
+		{
+			if(index <= 1)
+			{
+				return 1;
+			}
+
+			return CalculateFibonacci(index - 1) + CalculateFibonacci(index - 2);
+		}
+	}
+}
