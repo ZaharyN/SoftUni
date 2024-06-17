f0d29a9830fbeb78679c5542ad8dc08aa978aa58
ZaharyN <124094259+ZaharyN@users.noreply.github.com>
2024-06-17 23:36:56 +0300

    Recursion and combinatorial problems exercies

---
+++ C# Algorithms/03. Exercise - Recursion and Combinatorial Algorithms/01. Reverse array/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._Reverse_array
+{
+	internal class Program
+	{
+		public static int[] array;
+		public static bool isGenerated = false;
+		static void Main(string[] args)
+		{
+			array = Console.ReadLine()
+					.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+					.Select(int.Parse)
+					.ToArray();
+
+			ReverseArray(0);
+		}
+
+		private static void ReverseArray(int index)
+		{
+			if(index >= array.Length / 2)
+			{
+                Console.WriteLine(string.Join(" ", array));
+                return;
+			}
+			SwapPositions(index);
+			ReverseArray(index + 1);
+		}
+
+		private static void SwapPositions(int i)
+		{
+			int temp = array[array.Length - 1 - i];
+			array[array.Length - 1 - i] = array[i];
+			array[i] = temp;
+		}
+	}
+}
---
+++ C# Algorithms/03. Exercise - Recursion and Combinatorial Algorithms/02. Nested Loops To Recursion/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _02_
+{
+	internal class Program
+	{
+		public static int n;
+		public static int[] variations;
+		static void Main(string[] args)
+		{
+			n = int.Parse(Console.ReadLine());
+
+			variations = new int[n];
+			GenerateVectors(0);
+		}
+
+		private static void GenerateVectors(int index)
+		{
+			if(index >= n)
+			{
+                Console.WriteLine(string.Join(" ", variations));
+                return;
+			}
+
+			for (int i = 1; i <= n; i++)
+			{
+				variations[index] = i;
+				GenerateVectors(index + 1);
+			}
+		}
+	}
+}
