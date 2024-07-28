417618cd417a08c8ba0018ee45d2637928ec9628
ZaharyN <124094259+ZaharyN@users.noreply.github.com>
2024-07-28 17:56:47 +0300

    Binary search and Simple Sort algorithms added

---
+++ C# Algorithms/04. Searching, Sorting and Greedy Algorithms/01. Binary Search/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Binary_Search
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] array = Console.ReadLine()
+				.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+				.Select(int.Parse)
+				.ToArray();
+
+			int numberToFind = int.Parse(Console.ReadLine());
+
+			int numberToFindIndex = BinarySearch(array, numberToFind);
+			Console.WriteLine(numberToFindIndex);
+		}
+
+		private static int BinarySearch(int[] array, int numberToFind)
+		{
+			int leftIndex = 0;
+			int rightIndex = array.Length - 1;
+
+			while (leftIndex <= rightIndex)
+			{
+				int midIndex = (leftIndex + rightIndex) / 2;
+
+				if (array[midIndex] == numberToFind)
+				{
+					return midIndex;
+				}
+				if (array[midIndex] > numberToFind)
+				{
+					rightIndex = midIndex - 1;
+				}
+				else if (array[midIndex] < numberToFind)
+				{
+					leftIndex = midIndex + 1;
+				}
+			}
+			return -1;
+		}
+	}
+}
---
+++ C# Algorithms/04. Searching, Sorting and Greedy Algorithms/02. Selection Sort/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Selection_Sort
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] array = Console.ReadLine()
+				.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+				.Select(int.Parse)
+				.ToArray();
+
+			array = SelectionSort(array);
+            Console.WriteLine(string.Join(" ", array));
+        }
+
+		private static int[] SelectionSort(int[] array)
+		{
+			for (int i = 0; i < array.Length; i++)
+			{
+				int minNumberIndex = i;
+				for (int j = i + 1; j < array.Length; j++)
+				{
+					if (array[j] < array[minNumberIndex])
+					{
+						minNumberIndex = j;
+					}
+				}
+				SwapPositions(array, i, minNumberIndex);
+			}
+
+			return array;
+		}
+
+		private static void SwapPositions(int[] array, int index, int minNumberIndex)
+		{
+			int temp = array[index];
+			array[index] = array[minNumberIndex];
+			array[minNumberIndex] = temp;
+		}
+	}
+}
