84c60cd0d0220a8393d7f5da399c2b8e3c2b580e
ZaharyN <124094259+ZaharyN@users.noreply.github.com>
2024-08-10 23:53:59 +0300

    Added Bubble sort and Insertion sort algorithms

---
+++ C# Algorithms/04. Searching, Sorting and Greedy Algorithms/03. Bubble Sort/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bubble_Sort
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] array = Console.ReadLine()
+				.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+				.Select(int.Parse)
+				.ToArray();
+
+			array = BubbleSort(array);
+			Console.WriteLine(string.Join(" ", array));
+		}
+
+		private static int[] BubbleSort(int[] array)
+		{
+			bool isSorted = false;
+			int i = 0;
+
+			while (!isSorted)
+			{
+				isSorted = true;
+				for (int j = 1; j < array.Length - i; j++)
+				{
+					if (array[j - 1] > array[j])
+					{
+						SwapPositions(array, j - 1, j);
+						isSorted = false;
+					}
+				}
+				i++;
+			}
+
+			return array;
+		}
+
+		private static void SwapPositions(int[] array, int biggerNumberIndex, int smallerNumberIndex)
+		{
+			int temp = array[biggerNumberIndex];
+			array[biggerNumberIndex] = array[smallerNumberIndex];
+			array[smallerNumberIndex] = temp;
+		}
+	}
+}
---
+++ C# Algorithms/04. Searching, Sorting and Greedy Algorithms/04. Insertion Sort/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Insertion_Sort
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] array = Console.ReadLine()
+				.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+				.Select(int.Parse)
+				.ToArray();
+
+			array = InsertionSort(array);
+			Console.WriteLine(string.Join(" ", array));
+		}
+
+		private static int[] InsertionSort(int[] array)
+		{
+			for (int i = 1; i < array.Length; i++)
+			{
+				while (i > 0 && array[i] < array[i - 1])
+				{
+					SwapPositions(array, i, i - 1);
+					i--;
+				}
+			}
+
+			return array;
+		}
+
+		private static void SwapPositions(int[] array, int biggerNumberIndex, int smallerNumberIndex)
+		{
+			int temp = array[biggerNumberIndex];
+			array[biggerNumberIndex] = array[smallerNumberIndex];
+			array[smallerNumberIndex] = temp;
+		}
+	}
+}
