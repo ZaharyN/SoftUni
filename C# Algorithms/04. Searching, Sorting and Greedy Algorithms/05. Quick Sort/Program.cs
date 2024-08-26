b8fafd59f4e81622ebfa9923e81240b9bcb6c069
ZaharyN <124094259+ZaharyN@users.noreply.github.com>
2024-08-26 23:05:09 +0300

    QuickSort and MergeSort added

---
+++ C# Algorithms/04. Searching, Sorting and Greedy Algorithms/05. Quick Sort/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Quick_Sort
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] array = Console.ReadLine()
+				.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+				.Select(int.Parse)
+				.ToArray();
+
+			QuickSort(array, 0, array.Length - 1);
+			Console.WriteLine(string.Join(" ", array));
+		}
+
+		private static void QuickSort(int[] array, int start, int end)
+		{
+			if (start >= end)
+			{
+				return;
+			}
+			int pivot = start;
+			int left = start + 1;
+			int right = end;
+
+			while (left <= right)
+			{
+				if (array[left] > array[pivot] &&
+					array[right] < array[pivot])
+				{
+					Swap(array, left, right);
+				}
+				if (array[left] <= array[pivot])
+				{
+					left += 1;
+				}
+				if (array[right] >= array[pivot])
+				{
+					right -= 1;
+				}
+			}
+			Swap(array, pivot, right);
+			QuickSort(array, start, right);
+			QuickSort(array, left, end);
+		}
+
+		private static void Swap(int[] array, int left, int right)
+		{
+			int temp = array[left];
+			array[left] = array[right];
+			array[right] = temp;
+		}
+	}
+}
---
+++ C# Algorithms/04. Searching, Sorting and Greedy Algorithms/06. Merge Sort/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06._Merge_Sort
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] array = Console.ReadLine()
+				.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+				.Select(int.Parse)
+				.ToArray();
+
+			array = MergeSort(array);
+
+			Console.WriteLine(string.Join(" ", array));
+		}
+
+		private static int[] MergeSort(int[] array)
+		{
+			if (array.Length <= 1)
+			{
+				return array;
+			}
+
+			int[] left = array.Take(array.Length / 2).ToArray();
+			int[] right = array.Skip(array.Length / 2).ToArray();
+
+			return MergeArrays(MergeSort(left), MergeSort(right));
+		}
+
+		private static int[] MergeArrays(int[] left, int[] right)
+		{
+			int[] sorted = new int[left.Length + right.Length];
+
+			int startIndex = 0;
+			int leftIndex = 0;
+			int rightIndex = 0;
+
+			while(leftIndex < left.Length && rightIndex < right.Length)
+			{
+				if (left[leftIndex] < right[rightIndex])
+				{
+					sorted[startIndex++] = left[leftIndex++];
+				}
+				else
+				{
+				 	sorted[startIndex++] = right[rightIndex++];	
+				}
+			}
+
+			for (int i = leftIndex; i < left.Length; i++)
+			{
+				sorted[startIndex++] = left[i];
+			}
+
+			for (int i = rightIndex; i < right.Length; i++)
+			{
+				sorted[startIndex++] = right[i];
+			}
+			return sorted;
+		}
+	}
+}
