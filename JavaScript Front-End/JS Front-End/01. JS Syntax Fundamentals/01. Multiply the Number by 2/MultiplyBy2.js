3f037609c466d4359d3f1cff0f5e60f05dfb2fa4
ZaharyN <124094259+ZaharyN@users.noreply.github.com>
2024-10-30 21:44:12 +0200

    Js Syntaxt tasks

---
+++ JavaScript Front-End/JS Front-End/01. JS Syntax Fundamentals/01. Multiply the Number by 2/MultiplyBy2.js
@@ -0,0 +1,12 @@
+function solve (num){
+    console.log(num * 2);
+}
+
+solve(2);
+solve(5);
+solve(20);
+
+// Input	Output
+// 2	4
+// 5	10
+// 20	40
---
+++ JavaScript Front-End/JS Front-End/01. JS Syntax Fundamentals/03. Excellent grade/excellent-grade.js
@@ -0,0 +1,11 @@
+function solve(grade) {
+    if(grade >= 5.50 && grade <= 6.00){
+        console.log('Excellent');
+    }
+    else {
+        console.log('Not excellent');
+    }
+}
+
+solve(5.50);
+solve(4.35);
