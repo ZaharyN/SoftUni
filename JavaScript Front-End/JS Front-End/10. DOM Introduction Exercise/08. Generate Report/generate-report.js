91274fe3b9905bbd789aaac9801b408dc8766e88
ZaharyN <124094259+ZaharyN@users.noreply.github.com>
2024-12-05 23:19:37 +0200

    DOM Introduction exercise final

--- JavaScript Front-End/JS Front-End/10. DOM Introduction Exercise/08. Generate Report/generate-report.js
+++ JavaScript Front-End/JS Front-End/10. DOM Introduction Exercise/08. Generate Report/generate-report.js
@@ -1,3 +1,22 @@
 function solve() {
-    //TODO
+    const headers = document.querySelectorAll('thead tr th');
+    const allTableRows = document.querySelectorAll('tbody tr');
+
+    const outputEl = document.getElementById('output');
+    
+    const checkedHeaders = [...headers]
+    .map((h, i) => ({el: h.children[0], name: h.children[0].getAttribute('name'), index: i}))
+    .filter(x => x.el.checked);
+    
+    console.log(checkedHeaders);
+    
+    const outputData = [...allTableRows]
+        .map( row => {
+            return checkedHeaders.reduce( (acc, checkedHeader) => {
+                acc[checkedHeader.name] = row.children[checkedHeader.index].textContent;
+                return acc;
+            }, {});
+        })
+
+    outputEl.value = JSON.stringify(outputData);
 }
--- JavaScript Front-End/JS Front-End/10. DOM Introduction Exercise/09. Number Convertor/number-convertor.js
+++ JavaScript Front-End/JS Front-End/10. DOM Introduction Exercise/09. Number Convertor/number-convertor.js
@@ -1,3 +1,16 @@
 function solve() {
-    // TODO
+    const inputNumber = Number(document.querySelector('#input').value);
+    const convertOption = document.querySelector('#selectMenuTo').value.toLowerCase();
+    const result = document.querySelector('#result');
+
+    switch (convertOption) {
+        case 'binary':
+            result.value = inputNumber.toString(2);
+            break;
+        case 'hexadecimal':
+            result.value = inputNumber.toString(16).toUpperCase();
+            break;
+        default:
+            return;
+    }
 }
