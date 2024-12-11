942580e7834bb42b71d5a4b0c00e8cb4eae2b776
ZaharyN <124094259+ZaharyN@users.noreply.github.com>
2024-12-11 11:22:13 +0200

    Asynchronous programming task 2 added

--- JavaScript Front-End/JS Front-End/13. Asynchronous Programming/01.Bus-Stop/app.js
+++ JavaScript Front-End/JS Front-End/13. Asynchronous Programming/01.Bus-Stop/app.js
@@ -1,3 +1,26 @@
 function getInfo() {
-    // TODO:
+
+    const getUrl = "http://localhost:3030/jsonstore/bus/businfo/";
+    const input = document.querySelector('#stopInfo #stopId');
+    const submitButton = document.querySelector('#stopInfo #submit');
+    const busId = input.value;
+    const divName = document.querySelector('#result #stopName');
+    const busList = document.querySelector('#result #buses');
+    divName.innerHTML = '';
+    busList.innerHTML = '';
+
+    fetch(getUrl + busId)
+        .then(result => result.json())
+        .then(data => {
+            divName.textContent = data.name;
+
+            Object.entries(data.buses).forEach(([busId, time]) => {
+
+                const liEl = document.createElement('li');
+                liEl.textContent = `Bus ${busId} arrives in ${time} minutes`;
+                busList.append(liEl);
+
+            })
+        })
+        .catch(error => divName.textContent = 'Error');
 }
--- JavaScript Front-End/JS Front-End/13. Asynchronous Programming/02.Bus-Schedule/app.js
+++ JavaScript Front-End/JS Front-End/13. Asynchronous Programming/02.Bus-Schedule/app.js
@@ -1,10 +1,30 @@
 function solve() {
+    const infoBox = document.querySelector('span.info');
+    const departButton = document.querySelector('input#depart');
+    const arriveButton = document.querySelector('input#arrive');
+    let nextStopId = "depot";
+    let nextStopName = '';
+
     function depart() {
-        // TODO: 
+
+        const getUrl = 'http://localhost:3030/jsonstore/bus/schedule/';
+
+        fetch(getUrl + nextStopId)
+            .then(result => result.json())
+            .then(data => {
+                infoBox.textContent = `Next stop ${data.name}`;
+                nextStopId = data.next;
+                nextStopName = data.name;
+                departButton.disabled = true;
+                arriveButton.disabled = false;
+            })
+            .catch(err => infoBox.textContent = 'Error');
     }
 
     async function arrive() {
-        // TODO:
+        infoBox.textContent = `Arriving at ${nextStopName}`;
+        arriveButton.disabled = true;
+        departButton.disabled = false;
     }
 
     return {
